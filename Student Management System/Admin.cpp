e988c7bbf364666d397d51313b30dc2f826e127d
Diego Kalaw <kalawd@mcmaster.ca>
2025-03-24 17:13:56 -0400

    admin changes

---
+++ Student Management System/Admin.cpp
@@ -0,0 +1,37 @@
+#include "Admin.h"
+#include <iostream>
+
+using namespace std;
+
+Admin::admin() {
+    password = "password123";
+}
+
+bool Admin::login()
+{
+    string inputUsername, inputPassword;
+
+    while (true)
+    {
+        cout << "Enter Admin Username: ";
+        cin >> inputUsername;
+        cout << "Enter Password: ";
+        cin >> inputPassword;
+
+        if (inputUsername.size() >= 4 && inputUsername.substr(inputUsername.size() - 4) == "@mac")
+        {
+            if (inputPassword == password)
+            {
+                cout << "Login successful!\n";
+                return true;
+            }
+            else
+            {
+                cout << "Incorrect password! Try again.\n";
+            }
+        }
+        else {
+            cout << "Invalid username! It must end with '@mac'.\n";
+        }
+    }
+}
--- Student Management System/Admin.h
+++ Student Management System/Admin.h
@@ -1,1 +1,16 @@
-#pragma once
+#ifndef ADMIN_H
+#define ADMIN_H
+#include <string>
+using namespace std;
+class Admin
+{
+private:
+    string password;
+
+public:
+    admin(); // Constructor to set default credentials
+    bool login(); // Function to authenticate user
+};
+
+#endif
+
