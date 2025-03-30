f2bf55546caa4f958677c262db6d19d29a9ca10d
Kareem Aldulaimi <aldulaik@mcmaster.ca>
2025-03-30 18:34:33 -0400

    c

--- Student Management System/Timetable.h
+++ Student Management System/Timetable.h
@@ -1,20 +1,33 @@
 #pragma once
 #include <iostream>
-
+#include <string>
 
 using namespace std;
 
+class Timetable {
 
-class TimeTable {
+private:
 
-	string studentID;
 	string subjects[6];
-	string pushTEST;
+	string times[6];
+	int classCount;
 
 
 
 
+public:
 
+	Timetable(); //default contructor
+
+
+	//mutators
+
+	void add_class(string subject, string time);
+
+
+	void displayTimetable() const; //virutal function
 
 
 };
+
+void runTimetableMenu(); //not apart of object, helper fxn therefore declared outside class
